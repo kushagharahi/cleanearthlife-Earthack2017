5c3c5960b162559da0d270baab71539d65870384
Kusha Gharahi <kusha95@gmail.com>
2017-04-21 23:42:51 -0500

    started on entry point

---
+++ app/src/scripts/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+import App from 'App.vue'
+
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+  ]
+})
+
+router.beforeEach(function (to, from, next) {
+  document.title = to.meta.title
+  next()
+})
+
+const root = new Vue({
+  el: '#app',
+  router: router,
+  render: function(h){
+    return h(App)
+  },
+  replace: false
+})
