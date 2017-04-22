26e6e81c1a3301f0c3d52c19470c299a3a4874c2
Kusha Gharahi <kusha95@gmail.com>
2017-04-21 23:51:04 -0500

    started on home component

--- app/src/scripts/main.js
+++ app/src/scripts/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
-import App from 'App.vue'
+import App from './App.vue'
 
 Vue.use(VueRouter)
 Vue.use(VueResource)
