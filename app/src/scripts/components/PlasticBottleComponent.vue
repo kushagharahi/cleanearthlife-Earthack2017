0331d8a72fdd4091d544fc9c832f551e445db4dd
Kusha Gharahi <kusha95@gmail.com>
2017-04-22 21:30:24 -0500

    added interactive to light bulb

--- app/src/scripts/components/PlasticBottleComponent.vue
+++ app/src/scripts/components/PlasticBottleComponent.vue
@@ -3,7 +3,7 @@
 <i class="fa fa-tint" aria-hidden="true" style="font-size: 10em;"></i>
 		<h2>Reusable Water Bottles!</h2>
 		<p>Roughly only 20% of plastic bottles are recycled in the US.</p>
-        <p>For <input v-model="numRefills" style="width: 20px;" maxlength="2"/> refills of a 32oz standard water bottle, you would be saving {{Math.ceil(numRefills * 2 * .8)}} plastic bottles from the landfill.</p>
+        <p>For <input v-model="numRefills" style="width: 20px;" maxlength="2"/> refills of a 32oz standard water bottle, you would be saving <b>{{Math.ceil(numRefills * 2 * .8)}} plastic bottles from the landfill.</b></p>
 </div>
 </template>
 
