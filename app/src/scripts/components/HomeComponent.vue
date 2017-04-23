c8c27ed79c00acaa74359da9211692edc755ac63
Kusha Gharahi <kusha95@gmail.com>
2017-04-22 21:14:29 -0500

    added plastic bottle component

--- app/src/scripts/components/HomeComponent.vue
+++ app/src/scripts/components/HomeComponent.vue
@@ -4,18 +4,17 @@
         <div class='fullscreen' id='intro' style='background-color:#00e676'>
             <div id='component'>
                 <intro-component></intro-component>
-
             </div>
-                            <div class="bottom_aligner">
-                 <a href="#" v-on:click='scroll("water")'><i class="fa fa-angle-down" style="font-size:7em"></i></a>
-                 </div>
+            <div class="bottom_aligner">
+                 <a href="#water" v-on:click='scroll("water")'><i class="fa fa-angle-down" style="font-size:7em"></i></a>
+            </div>
         </div>
         <div class='fullscreen' id='water' style='background-color:#00b0ff'>
             <div id='component'>
 			    <water-component></water-component>
             </div>
             <div class="bottom_aligner">
-                 <a href="#" v-on:click='scroll("pollution")'><i class="fa fa-angle-down" style="font-size:7em"></i></a>
+                 <a href="#pollution" v-on:click='scroll("pollution")'><i class="fa fa-angle-down" style="font-size:7em"></i></a>
                  </div>
         </div>
         <div class='fullscreen' id='pollution' style='background-color:#bdbdbd'>
@@ -23,7 +22,7 @@
 			    <pollution-component></pollution-component>
             </div>
             <div class="bottom_aligner">
-                 <a href="#" v-on:click='scroll("lights")'><i class="fa fa-angle-down" style="font-size:7em"></i></a>
+                 <a href="#lights" v-on:click='scroll("lights")'><i class="fa fa-angle-down" style="font-size:7em"></i></a>
                  </div>
         </div>
         <div class='fullscreen' id='lights' style='background-color:#ffea00'>
@@ -31,7 +30,15 @@
 			    <lights-component></lights-component>
             </div>
             <div class="bottom_aligner">
-                 <a href="#" v-on:click='scroll("conclusion")'><i class="fa fa-angle-down" style="font-size:7em"></i></a>
+                 <a href="#plastic" v-on:click='scroll("plasticBottle")'><i class="fa fa-angle-down" style="font-size:7em"></i></a>
+                 </div>
+        </div>
+           <div class='fullscreen' id='plasticBottle' style='background-color:#f50057'>
+            <div id='component'>
+			    <plastic-bottle-component></plastic-bottle-component>
+            </div>
+            <div class="bottom_aligner">
+                 <a href="#conclusion" v-on:click='scroll("conclusion")'><i class="fa fa-angle-down" style="font-size:7em"></i></a>
                  </div>
         </div>
         <div class='fullscreen' id='conclusion' style='background-color:#651fff'>
@@ -39,7 +46,7 @@
 			    <conclusion-component></conclusion-component>
             </div>
             <div class="bottom_aligner">
-                 <a href="#" v-on:click='scroll("intro")'><i class="fa fa-angle-down" style="font-size:7em"></i></a>
+                 <a href="#intro" v-on:click='scroll("intro")'><i class="fa fa-angle-down" style="font-size:7em"></i></a>
                  </div>
         </div>
 	</div>
@@ -54,9 +61,10 @@ import WaterComponent from './WaterComponent.vue'
 import PollutionComponent from './PollutionComponent.vue'
 import ConclusionComponent from './ConclusionComponent.vue'
 import LightsComponent from './LightsComponent.vue'
+import PlasticBottleComponent from './PlasticBottleComponent.vue'
 
 export default {
-  components: { NavComponent, HomeComponent, LoginComponent, IntroComponent, PollutionComponent, WaterComponent, ConclusionComponent, LightsComponent },
+  components: { NavComponent, HomeComponent, LoginComponent, IntroComponent, PollutionComponent, WaterComponent, ConclusionComponent, LightsComponent, PlasticBottleComponent },
   methods: {
     scroll: function (id) {
       this.$SmoothScroll(document.getElementById(id))
@@ -70,16 +78,14 @@ export default {
     width: 100%; 
     height: 100vh;
     display: flex;
-      justify-content: center;
-
-      align-items: center;
-
+    justify-content: center;
+    align-items: center;
 }
 
 .bottom_aligner {
-  display: inline-block;
-align-self: flex-end;
-margin-top: auto;
+    display: inline-block;
+    align-self: flex-end;
+    margin-top: auto;
 }
 
 #component {
